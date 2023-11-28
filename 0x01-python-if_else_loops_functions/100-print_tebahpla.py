bbee2450b77727c6a32160f06b9f6f070e1adb75
OtmaneTachalte <otmanetachalte@gmail.com>
2023-11-28 14:22:21 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-toggle_case = 0
-for ascii_val in range(ord('z'), ord('a') - 1, -1):
-    adjusted_ascii = ascii_val - toggle_case
-    print(chr(adjusted_ascii), end='')
-    toggle_case = 0 if toggle_case else 32  # Toggle between 0 and 32
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
