38932e458995c68218625e93103e355f1ea227d1
OtmaneTachalte <otmanetachalte@gmail.com>
2023-12-06 12:00:46 -0800

    correction

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,15 +6,3 @@ def square_matrix_simple(matrix=[]):
 
     # Using map and lambda to create a new matrix with squared values
     return [list(map(lambda x: x ** 2, row)) for row in matrix]
-
-# Test the function
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    new_matrix = square_matrix_simple(matrix)
-    print(new_matrix)
-    print(matrix)
