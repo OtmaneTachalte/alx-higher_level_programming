36504345c5fa4ed476e3044c3e5d37e12b2bbf56
OtmaneTachalte <otmanetachalte@gmail.com>
2023-12-19 12:54:52 -0800

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+class Square:
+
+    def __init__(self, size=0):
+        """Initialize a new Square with optional size.
+
+        Args:
+            size (int, optional): The size of the new square. Defaults to 0.
+
+        Raises:
+            TypeError: If 'size' is not an integer.
+            ValueError: If 'size' is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
